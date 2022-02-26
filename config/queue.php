e842f40e8f22097c549a638ce3fb621dc3733f66
Nikita Krochak <krch@feip.co>
2022-02-26 12:17:25 +1000

    Add second RabbitMQ host, use Horizon worker

--- config/queue.php
+++ config/queue.php
@@ -75,11 +75,18 @@ return [
 
             'driver' => 'rabbitmq',
             'queue' => env('RABBITMQ_QUEUE', 'default'),
-            'connection' => PhpAmqpLib\Connection\AMQPLazyConnection::class,
+            'connection' => PhpAmqpLib\Connection\AMQPSocketConnection::class,
 
             'hosts' => [
                 [
                     'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+                [
+                    'host' => env('RABBITMQ_DOCKER_HOST', '127.0.0.1'),
                     'port' => env('RABBITMQ_PORT', 5672),
                     'user' => env('RABBITMQ_USER', 'guest'),
                     'password' => env('RABBITMQ_PASSWORD', 'guest'),
